4304f80b022452f3987fd011dc6c3cf7e58fa090
ianabreu <ianabreudev@gmail.com>
2022-12-01 06:23:12 -0300

    criando animacoes de entrada dos elementos com javascript

--- js/main.js
+++ js/main.js
@@ -25,3 +25,42 @@ function displayText(idActive, idInactive) {
         else menuColor.classList.remove('menuFixed');
     });
 })();
+
+
+const debounce = function(func, wait, immediate) {
+	let timeout;
+	return function(...args) {
+	  const context = this;
+	  const later = function () {
+		timeout = null;
+		if (!immediate) func.apply(context, args);
+	  };
+	  const callNow = immediate && !timeout;
+	  clearTimeout(timeout);
+	  timeout = setTimeout(later, wait);
+	  if (callNow) func.apply(context, args);
+	};
+  };
+
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+const windowTop = window.scrollY + (window.innerHeight * 0.75);
+target.forEach(function(item) {
+	if ((windowTop) > item.offsetTop) {
+		item.classList.add(animationClass);
+	} else {
+		item.classList.remove(animationClass);
+	}
+})
+}
+animeScroll();
+
+if (target.length) {
+	window.addEventListener('scroll', debounce(function() {
+		animeScroll();
+		console.log(12);
+	}, 50));
+}
