7476925df9edc6cf2fc512d650ecfdb62753c615
ianabreu <ianabreudev@gmail.com>
2022-12-03 00:54:25 -0300

    configurando modal

--- js/main.js
+++ js/main.js
@@ -74,7 +74,7 @@ function renderProject() {
 		link.classList.add('area-img');
 		link.id = `project_${item.id}`;
 		link.onclick = function() {
-			displayModal(item.id);
+			displayModal(item);
 		}
 
 		let img = document.createElement('img');
@@ -88,6 +88,63 @@ function renderProject() {
 }
 renderProject();
 
-function displayModal(id) {
-alert(projects[id].name)
+const modal = document.getElementById('modal');
+
+function closeModal() {
+	modal.style.display = 'none';
 }
+
+function displayModal(project) {
+modal.style.display = 'flex';
+montaItemModal(project)
+showSlides(slideIndex);
+}
+
+function montaItemModal(project) {
+	let title = document.getElementById('title');
+	let description = document.getElementById('description');
+	title.innerText = project.name;
+	description.innerText = project.description;
+	project.images.forEach((url) => {
+		montaImageSlider(url);
+	})
+}
+
+function montaImageSlider(url) {
+	const slidesContainer = document.getElementById('slides-container');
+	let div = document.createElement('div');
+	let img = document.createElement('img');
+	div.classList = 'mySlides fade image';
+	img.src = url;
+	img.style = 'width:100%';
+	div.appendChild(img);
+	slidesContainer.appendChild(div);
+}
+
+
+	let slideIndex = 1;
+	
+	
+	function plusSlides(n) {
+		showSlides(slideIndex += n);
+	}
+	
+	function currentSlide(n) {
+		showSlides(slideIndex = n);
+	}
+	
+	function showSlides(n) {
+		let i;
+		let slides = document.getElementsByClassName("mySlides");
+		let dots = document.getElementsByClassName("dot");
+		if (n > slides.length) {slideIndex = 1}
+		if (n < 1) {slideIndex = slides.length}
+		for (i = 0; i < slides.length; i++) {
+			slides[i].style.display = "none";
+		}
+		for (i = 0; i < dots.length; i++) {
+			dots[i].className = dots[i].className.replace(" active", "");
+		}
+		slides[slideIndex-1].style.display = "block";
+		dots[slideIndex-1].className += " active";
+	}
