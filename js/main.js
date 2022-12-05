d18e583b7cba50a79497e0e7b0b45efe1d84047f
ianabreu <ianabreudev@gmail.com>
2022-12-04 21:51:52 -0300

    ajustando projeto na sectios Projetos

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ function displayText(idActive, idInactive) {
 	let active = document.getElementById(idActive);
 	let inactive = document.getElementById(idInactive);
 	let arrayH6 = document.querySelectorAll('.btn-h6');
-	
+
 	active.style.display = 'block';
 	inactive.style.display = 'none';
-	
+
 	if (idActive === 'biography') {
 		arrayH6[0].classList.add("active");
 		arrayH6[1].classList.remove("active");
@@ -20,46 +20,46 @@ function displayText(idActive, idInactive) {
 
 (function () {
 	let menuColor = document.getElementById('menuColor');
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 50 ) menuColor.classList.add('menuFixed'); 
-        else menuColor.classList.remove('menuFixed');
-    });
+	window.addEventListener('scroll', () => {
+		if (window.scrollY > 50) menuColor.classList.add('menuFixed');
+		else menuColor.classList.remove('menuFixed');
+	});
 })();
 
 
-const debounce = function(func, wait, immediate) {
+const debounce = function (func, wait, immediate) {
 	let timeout;
-	return function(...args) {
-	  const context = this;
-	  const later = function () {
-		timeout = null;
-		if (!immediate) func.apply(context, args);
-	  };
-	  const callNow = immediate && !timeout;
-	  clearTimeout(timeout);
-	  timeout = setTimeout(later, wait);
-	  if (callNow) func.apply(context, args);
+	return function (...args) {
+		const context = this;
+		const later = function () {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		const callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
 	};
-  };
+};
 
 
-const target = document.querySelectorAll('[data-anime]');
+let target = document.querySelectorAll('[data-anime]');
 const animationClass = 'animate';
 
 function animeScroll() {
-const windowTop = window.scrollY + (window.innerHeight * 0.75);
-target.forEach(function(item) {
-	if ((windowTop) > item.offsetTop) {
-		item.classList.add(animationClass);
-	} else {
-		item.classList.remove(animationClass);
-	}
-})
+	const windowTop = window.scrollY + (window.innerHeight * 0.75);
+	target.forEach(function (item) {
+		if ((windowTop) > item.offsetTop) {
+			item.classList.add(animationClass);
+		} else {
+			item.classList.remove(animationClass);
+		}
+	})
 }
 animeScroll();
 
 if (target.length) {
-	window.addEventListener('scroll', debounce(function() {
+	window.addEventListener('scroll', debounce(function () {
 		animeScroll();
 	}, 50));
 }
@@ -71,21 +71,27 @@ const areaProjects = document.querySelector('#content-projects')
 function renderProject() {
 	projects.forEach((item) => {
 		let link = document.createElement('a');
-		link.classList.add('area-img');
+		// link.classList.add('area-img');
+		link.dataset.anime = 'top';
 		link.id = `project_${item.id}`;
-		link.onclick = function() {
+		link.onclick = function () {
 			displayModal(item);
 		}
+		let div = document.createElement('div');
+		div.classList.add('area-img');
 
 		let img = document.createElement('img');
 		img.src = `${item.url}`;
 		img.alt = `${item.name}: ${item.description}`
 
-		
-		link.appendChild(img);
+
+		div.appendChild(img);
+		link.appendChild(div);
 		areaProjects.appendChild(link);
 	});
+	target = document.querySelectorAll('[data-anime]');
 }
+
 renderProject();
 
 const modal = document.getElementById('modal');
@@ -95,56 +101,60 @@ function closeModal() {
 }
 
 function displayModal(project) {
-modal.style.display = 'flex';
-montaItemModal(project)
-showSlides(slideIndex);
+	modal.style.display = 'flex';
+	createItemModal(project)
+	showSlides(slideIndex);
 }
 
-function montaItemModal(project) {
+function createItemModal(project) {
 	let title = document.getElementById('title');
-	let description = document.getElementById('description');
 	title.innerText = project.name;
+
+	let description = document.getElementById('description');
 	description.innerText = project.description;
+
 	project.images.forEach((url) => {
-		montaImageSlider(url);
+		createImagesSlider(url);
 	})
+	let linkButton = document.getElementById('linkButton')
+	let githubButton = document.getElementById('githubButton')
+	linkButton.href = project.github
+	githubButton.href = project.github
 }
 
-function montaImageSlider(url) {
+function createImagesSlider(url) {
 	const slidesContainer = document.getElementById('slides-container');
 	let div = document.createElement('div');
+	div.classList = 'mySlides fade';
+
 	let img = document.createElement('img');
-	div.classList = 'mySlides fade image';
 	img.src = url;
 	img.style = 'width:100%';
+
 	div.appendChild(img);
 	slidesContainer.appendChild(div);
 }
 
+let slideIndex = 1;
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+function showSlides(n) {
+	let i;
+	let slides = document.getElementsByClassName("mySlides");
+	let dots = document.getElementsByClassName("dot");
+	if (n > slides.length) { slideIndex = 1 }
+	if (n < 1) { slideIndex = slides.length }
 
-	let slideIndex = 1;
-	
-	
-	function plusSlides(n) {
-		showSlides(slideIndex += n);
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
 	}
-	
-	function currentSlide(n) {
-		showSlides(slideIndex = n);
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active-dot", "");
 	}
-	
-	function showSlides(n) {
-		let i;
-		let slides = document.getElementsByClassName("mySlides");
-		let dots = document.getElementsByClassName("dot");
-		if (n > slides.length) {slideIndex = 1}
-		if (n < 1) {slideIndex = slides.length}
-		for (i = 0; i < slides.length; i++) {
-			slides[i].style.display = "none";
-		}
-		for (i = 0; i < dots.length; i++) {
-			dots[i].className = dots[i].className.replace(" active", "");
-		}
-		slides[slideIndex-1].style.display = "block";
-		dots[slideIndex-1].className += " active";
-	}
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " active-dot";
+}
