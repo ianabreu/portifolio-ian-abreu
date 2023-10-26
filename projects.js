a414458e64124367c27bddfa567f56757abd2965
ianabreu <ianabreudev@gmail.com>
2023-10-26 20:41:13 -0300

    feat: add tech store to projects.js

--- projects.js
+++ projects.js
@@ -1,6 +1,21 @@
 const projects = [
   {
     id: 0,
+    name: "Tech Store",
+    description:
+      "Projeto de um e-commerce desenvolvido em NextJs, Typescript, Prisma e PostgreSQL, com autenticação do google, listagem de categorias e produtos, carrinho de compras e pagamentos com Stripe.",
+    category: "Web",
+    images: [
+      "./assets/tech-store/img0.jpg",
+      "./assets/tech-store/img1.jpg",
+      "./assets/tech-store/img2.jpg",
+    ],
+    url: "./assets/tech-store/cover.png",
+    demo: "https://tech-store-e-commerce.vercel.app/",
+    github: "https://github.com/ianabreu/e-commerce",
+  },
+  {
+    id: 1,
     name: "Meu Portfólio",
     description:
       "Este é o meu portfólio pessoal, onde compartilho informações sobre mim, meus projetos e minhas habilidades como desenvolvedor. Utilizei no seu desenvolvimento HTML, CSS e Javascript.",
@@ -17,7 +32,7 @@ const projects = [
     github: "https://github.com/ianabreu/portifolio-ian-abreu",
   },
   {
-    id: 1,
+    id: 2,
     name: "Popcine",
     description:
       "Um aplicativo para encontrar informações sobre filmes populares, próximos lançamentos, avaliações e salvar como favorito.",
